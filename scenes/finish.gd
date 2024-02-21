77d5a078ec795514fd58adbf92d84a16b5d4d6df
Brian Kelly <polymonic@gmail.com>
2024-02-20 22:19:45 -0600

    Switch to a deferred call for changing the scene

--- scenes/finish.gd
+++ scenes/finish.gd
@@ -4,4 +4,4 @@ extends Area2D
 
 func _on_body_entered(body):
 	if (body.name == "CharacterBody2D"):
-		get_tree().change_scene_to_packed(target_level)
+		get_tree().call_deferred("change_scene_to_packed", target_level)
